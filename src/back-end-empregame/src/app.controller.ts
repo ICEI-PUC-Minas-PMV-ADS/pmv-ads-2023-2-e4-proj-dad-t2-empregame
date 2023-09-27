33ad044bb1a25a04fccd4d4f8e392214732be006
ThiagoTerrinha <thiago.terra13@gmail.com>
2023-09-26 23:13:17 -0300

    add: rota de versão

--- src/back-end-empregame/src/app.controller.ts
+++ src/back-end-empregame/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('version')
   getHello(): { version: string; description: string } {
     return this.appService.getHello();
   }
