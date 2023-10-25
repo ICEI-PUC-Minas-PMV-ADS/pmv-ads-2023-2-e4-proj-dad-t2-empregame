386b19442c3e4beca91b7c563a6ed43d8b207cb4
ThiagoTerrinha <thiago.terra13@gmail.com>
2023-10-25 19:35:10 -0300

    add: configuração de cors e rota de update de senha

--- src/back-end-empregame/src/app/auth/auth.controller.ts
+++ src/back-end-empregame/src/app/auth/auth.controller.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 import { signInDto } from './dto/sign-in.dto';
 import { Public } from '../../utils/decorators/is-public.decorator';
 import { AuthRedefinirSenhaDto } from './dto/redefinir-senha.dto';
+import { AuthUpdateSenhaDto } from './dto/update-senha.dto';
+import { AuthUser, IAuthUser } from '../../utils/decorators/auth.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -17,8 +20,19 @@ export class AuthController {
 
   @Post('redefinir-senha')
   @Public()
-  async handle(@Body() data: AuthRedefinirSenhaDto) {
+  async redefinirSenha(@Body() data: AuthRedefinirSenhaDto) {
     await this.authService.redefinirSenha(data);
+
+    return;
+  }
+
+  @ApiBearerAuth()
+  @Post('update-senha')
+  async updateSenha(
+    @Body() data: AuthUpdateSenhaDto,
+    @AuthUser() user: IAuthUser,
+  ) {
+    await this.authService.updateSenha(user.usuario.id, data);
 
     return;
   }
