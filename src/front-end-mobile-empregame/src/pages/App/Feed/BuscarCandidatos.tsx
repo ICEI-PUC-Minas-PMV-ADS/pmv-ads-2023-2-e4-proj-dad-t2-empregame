424ee871ff23e8a7bb72d0ed4ae37613046af872
ThiagoTerrinha <thiago.terra13@gmail.com>
2023-11-26 22:53:39 -0300

    add: filtro dos candidatos

--- src/front-end-mobile-empregame/src/pages/App/Feed/BuscarCandidatos.tsx
+++ src/front-end-mobile-empregame/src/pages/App/Feed/BuscarCandidatos.tsx
@@ -1,15 +1,26 @@
-import { Box, FlatList, Spinner, View } from "native-base";
-import CardVaga from "../../../components/card-vaga";
-import { useCallback, useEffect, useState } from "react";
+import {
+  Box,
+  FlatList,
+  HStack,
+  Input,
+  Select,
+  Spinner,
+  VStack,
+  View,
+} from "native-base";
+import { useCallback, useState } from "react";
 import { useFetch } from "../../../utils/hooks/useFetch";
-import { IVaga } from "../../../interface/IVaga";
 import Toast from "react-native-toast-message";
 import { RefreshControl } from "react-native";
 import { IUsuario } from "../../../interface/IUsuario";
 import CardCandidato from "../../../components/card-candidato";
+import { IconLupa } from "../../../components/icons";
+import { SelectSecondary } from "../../../components/select-secondary";
 
 export const BuscarCandidatos = ({ navigation }: any) => {
   const [refreshing, setRefreshing] = useState(false);
+  const setHardskillFilter = new Set();
+  const setSoftskillFilter = new Set();
   const [pesquisa, setPesquisa] = useState<string | null>(null);
   const [hardskill, setHardskill] = useState<string | null>(null);
   const [softskill, setSoftskill] = useState<string | null>(null);
@@ -35,8 +46,80 @@ export const BuscarCandidatos = ({ navigation }: any) => {
     }, 2000);
   }, []);
 
+  const { data: hardskills } = useFetch<IHardskill[]>("/hardskills", {
+    onError: (err) => {
+      if (err.response?.data)
+        Toast.show({ text1: err.response.data.message, type: "error" });
+    },
+  });
+
+  const filterHardskills = hardskills?.filter((hardskill) => {
+    const duplicatedHardskills = setHardskillFilter.has(hardskill.nome);
+    setHardskillFilter.add(hardskill.nome);
+    return !duplicatedHardskills;
+  });
+
+  const { data: softskills } = useFetch<ISoftskill[]>("/softskills", {
+    onError: (err) => {
+      if (err.response?.data)
+        Toast.show({ text1: err.response.data.message, type: "error" });
+    },
+  });
+
+  const filterSoftskills = softskills?.filter((softskill) => {
+    const duplicatedSoftskills = setSoftskillFilter.has(softskill.nome);
+    setSoftskillFilter.add(softskill.nome);
+    return !duplicatedSoftskills;
+  });
+
   return (
     <View flex={1}>
+      <VStack space={"18px"} paddingX={"15px"} paddingY={"20px"}>
+        <Input
+          fontFamily={"Outfit-500"}
+          placeholder={"Pesquisar Candidato"}
+          onChangeText={(e) => setPesquisa(e)}
+          py={"10px"}
+          px={"25px"}
+          placeholderTextColor={"#ADADAD"}
+          color={"#2E2E2E"}
+          backgroundColor={"white"}
+          rounded={"full"}
+          fontSize={"16px"}
+          fontWeight={"medium"}
+          InputRightElement={
+            <Box paddingRight={"20px"}>
+              <IconLupa />
+            </Box>
+          }
+        />
+        <HStack space={"10px"}>
+          <SelectSecondary
+            placeholder="Hardskill"
+            onValueChange={(e) => setHardskill(e)}
+          >
+            {filterHardskills?.map((hardskill) => (
+              <Select.Item
+                key={hardskill.id + hardskill.nome}
+                value={hardskill.nome}
+                label={hardskill.nome}
+              />
+            ))}
+          </SelectSecondary>
+          <SelectSecondary
+            placeholder="SoftSkill"
+            onValueChange={(e) => setSoftskill(e)}
+          >
+            {filterSoftskills?.map((softskill) => (
+              <Select.Item
+                key={softskill.id + softskill.nome}
+                value={softskill.nome}
+                label={softskill.nome}
+              />
+            ))}
+          </SelectSecondary>
+        </HStack>
+      </VStack>
       {isFetching == true ? (
         <View flex={1} justifyContent={"center"}>
           <Spinner size="lg" />
@@ -45,7 +128,6 @@ export const BuscarCandidatos = ({ navigation }: any) => {
         <FlatList
           ItemSeparatorComponent={() => <Box h={"20px"} />}
           paddingX={"15px"}
-          paddingTop={"20px"}
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
